27d09df4f550f9ddc4b2a5c5c661534163482713
Juan Camilo Bohorquez Escalante <juan@pager.com>
2015-09-21 15:55:12 -0500

    updated node & nvm versions

--- scripts/install_nvm.sh
+++ scripts/install_nvm.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Installs the Node Version Manager (https://github.com/creationix/nvm)
 
-su vagrant -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.22.1/install.sh | bash"
+su vagrant -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash"
 
 # Load nvm for the current script
 export NVM_DIR="/home/vagrant/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 
-export NODE_VERSION='0.10.35'
+export NODE_VERSION='0.12.7'
 nvm install $NODE_VERSION
 nvm alias default $NODE_VERSION
 
